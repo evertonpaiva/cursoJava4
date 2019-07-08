ec712dc6c61c30e301c1da614acfd19671fb7dc5
Everton Paiva <evertonpaiva@gmail.com>
2019-07-08 11:17:28 -0300

    Testando exceções. Try, catch. Diferenças entre exceções checked e unchecked.

---
+++ java-pilha/src/Fluxo.java
@@ -0,0 +1,27 @@
+
+public class Fluxo {
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        try {
+        	metodo1();
+        } catch (ArithmeticException | NullPointerException | MinhaException ex) {
+        	String msg = ex.getMessage();
+			System.out.println(ex.getClass() + ": " + msg);
+		} 
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() throws MinhaException {
+        System.out.println("Ini do metodo1");        
+    	metodo2();	                
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() throws MinhaException {
+        System.out.println("Ini do metodo2");
+        
+        throw new MinhaException("Erro de cálculo");
+        
+        //System.out.println("Fim do metodo2");
+    }
+}
